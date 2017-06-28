b2e9e8f85249609401defcbdc39d538cf8f891a9
gokkulasudanr92 <gokkulasudanr92@gmail.com>
2017-06-28 16:04:44 -0400

    Microservice skeleton

--- src/main/java/org/renci/scidas/controller/MainController.java
+++ src/main/java/org/renci/scidas/controller/MainController.java
@@ -1,16 +1,18 @@
 package org.renci.scidas.controller;
 
+import javax.ws.rs.Produces;
+
+import org.renci.scidas.pojo.Test;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/helloWorld")
-public class HelloWorldController {
+@RequestMapping("/")
+public class MainController {
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	/*@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello(ModelMap model) {
 		model.addAttribute("msg", "JCG Hello World!");
 		return "helloWorld";
@@ -20,6 +22,16 @@ public class HelloWorldController {
 	public String displayMessage(@PathVariable String msg, ModelMap model) {
 		model.addAttribute("msg", msg);
 		return "helloWorld";
+	}*/
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	@Produces("application/json")
+	@ResponseBody
+	public Test test() {
+		Test data = new Test();
+		data.setName("Network Optimizer");
+		data.setDescription("This is a test POJO Object");
+		return data;
 	}
 	
 }
